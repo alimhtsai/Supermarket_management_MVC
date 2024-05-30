f46fcf8a020a10a0d4a77ab82d7e4fb6cfbe7438
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-30 13:27:23 -0700

    add Models

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -7,6 +8,12 @@ namespace WebApp.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var category = new Category{ CategoryId = id.HasValue ? id.Value : 0 };
+            return View(category);
         }
     }
 }
---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,9 @@
+namespace WebApp.Models
+{
+    public class Category
+    {
+        public int CategoryId {get; set;}
+        public string Name {get; set;} = string.Empty;
+        public string Description {get; set;} = string.Empty;
+    }
+}
