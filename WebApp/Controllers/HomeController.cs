10fc4792afda615596db7addefedeff419881561
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-30 13:04:52 -0700

    add controllers and views

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
