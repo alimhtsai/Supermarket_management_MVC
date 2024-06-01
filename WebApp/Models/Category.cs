c03c1b02bd962104715e5d1b3b9f1911d3f87f13
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-31 17:02:06 -0700

    add form validation with DataAnnotation

--- WebApp/Models/Category.cs
+++ WebApp/Models/Category.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApp.Models
 {
     public class Category
     {
         public int CategoryId {get; set;}
+        [Required]
         public string Name {get; set;} = string.Empty;
+        [Required]
         public string Description {get; set;} = string.Empty;
     }
 }
