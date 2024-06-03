f19397e18864fd0724ec38681985da5f7b61ca7e
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-06-02 17:31:14 -0700

    implement add product with ViewModel

--- WebApp/Controllers/ProductsController.cs
+++ WebApp/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers
 {
@@ -10,6 +11,27 @@ namespace WebApp.Controllers
         {
             var products = ProductsRepository.GetProducts(loadCategory: true);
             return View(products);
+        }
+
+        public IActionResult Add()
+        {
+            var productViewModel = new ProductViewModel
+            {
+                Categories = CategoriesRepository.GetCategories()
+            };
+            return View(productViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductsRepository.AddProduct(productViewModel.Product);
+                return RedirectToAction(nameof(Index));
+            }
+            productViewModel.Categories = CategoriesRepository.GetCategories();
+            return View(productViewModel);
         }
     }
 }
