fbee20568785ffbc87d322373dd8a15e88c3a7b1
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-06-03 17:21:46 -0700

    validate quantity value is less than the QuantityToSell with ValidationAttribute

--- WebApp/ViewModels/SalesViewModel.cs
+++ WebApp/ViewModels/SalesViewModel.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using WebApp.Models;
+using WebApp.ViewModels.Validations;
 
 namespace WebApp.ViewModels
 {
@@ -11,6 +12,7 @@ namespace WebApp.ViewModels
         
         [Display(Name = "Quantity")]
         [Range(1, int.MaxValue)]
+        [SalesViewModel_EnsureProperQuantity]
         public int QuantityToSell { get; set; }
     }
 }
